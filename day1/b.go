48d72f38709d5f0a34693b25cb0f37b24d72489c
k1nho <kinhong99@gmail.com>
2023-12-01 15:51:31 -0500

    day 1

---
+++ day1/b.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	// Read file argument
+	args := os.Args
+	// Get filename input
+	if len(args) != 2 {
+		panic("no file provided")
+	}
+	filename := args[1]
+	// Open the file
+	file, err := os.Open(filename)
+	if err != nil {
+		panic("could not open file")
+	}
+	defer file.Close()
+
+	// get the file content
+	fileContent, err := io.ReadAll(file)
+	if err != nil {
+		panic("could not read filecontent")
+	}
+	// transform into string
+	data := string(fileContent)
+	solve(data)
+}
+
+func solve(data string) {
+	lines := strings.Split(data, "\n")
+	// ignore last entry (empty line)
+	lines = lines[:len(lines)-1]
+
+	numbers := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	res := int64(0)
+	for _, line := range lines {
+		var digits []string
+		for i, ch := range line {
+			if unicode.IsDigit(ch) {
+				digits = append(digits, fmt.Sprintf("%c", ch))
+			}
+			for j, number := range numbers {
+				if strings.HasPrefix(line[i:], number) {
+					digits = append(digits, fmt.Sprint(j+1))
+				}
+			}
+		}
+		val, err := strconv.ParseInt(digits[0]+digits[len(digits)-1], 10, 64)
+		if err != nil {
+			panic("no val")
+		}
+		res += val
+	}
+
+	fmt.Print("res: ", res)
+}
